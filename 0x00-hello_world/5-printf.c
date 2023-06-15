511dbc8a9c1dea905bb0a14cb83bf13b06de893b
moncef63 <moncif.ayouch@gmail.com>
2023-06-15 16:05:52 -0700

    adding betty documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+/**
+* main - Entry point
+* with proper grammar, but the outcome is a piece of art
+* Return: 0
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
